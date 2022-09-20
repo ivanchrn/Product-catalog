30c1e7c9f7e94ecf9b2861d2fa0dcb16f7eadc40
Ivan <ivans.cornijs@gmail.com>
2022-09-20 07:39:14 +0300

    WIP

--- php/addConfig.php
+++ php/addConfig.php
@@ -4,13 +4,13 @@ require_once ("database.php");
 
 class addConfig{
 
-    private $id;
-    private $sku;
-    private $name;
-    private $price;
+    protected $id;
+    protected $sku;
+    protected $name;    
+    protected $price;
     protected $dbCnx;
 
-    public function __construct($id=0,$sku="",$name="",$price=""){
+    public function __construct($id="",$sku="",$name="",$price=""){
         $this->id = $id;
         $this->sku = $sku;
         $this->name = $name;
@@ -46,13 +46,14 @@ class addConfig{
 
     public function insertData(){
         try{
-            $stm = $this->dbCnx->prepare("INSERT INTO products(sku,name,price) VALUES (?,?,?)");
-            $stm->execute([$this->sku, $this->name, $this->price]);
+            $stm = $this->dbCnx->prepare("INSERT INTO products(id,sku,name,price) VALUES (?,?,?,?)");
+            $stm->execute([$this->id, $this->sku, $this->name, $this->price]);
         }
         catch(Expection $e){
             return $e->getMessage();
         }
     }
+
     public function fetchAll(){
         try{
             $stm = $this->dbCnx->prepare("SELECT * FROM products");
@@ -63,6 +64,7 @@ class addConfig{
             return $e->getMessage();
         }
     }
+
     public function fetchOne(){
         try{
             $stm = $this->dbCnx->prepare("SELECT * FROM products WHERE id=?");
@@ -73,6 +75,7 @@ class addConfig{
             return $e->getMessage();
         }
     }
+
     public function delete(){
         try{
             $stm = $this->dbCnx->prepare("DELETE FROM products WHERE id=?");
@@ -82,6 +85,113 @@ class addConfig{
         catch(Expection $e){
             return $e->getMessage();
         }
+    }
+}
+
+class Dvd extends addConfig {
+    private $size; 
+    private $product_id;
+
+    public function __construct($id="", $sku="",$name="",$price="", $size="", $product_id=""){
+        parent::__construct($id="",$sku="",$name="",$price="");
+
+        $this->product_id = $product_id;
+        $this->size = $size;
+
+        $this->dbCnx = new PDO(DB_TYPE.":host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PWD,[ PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+    }
+
+    public function insertData(){
+        try{
+            $stm = $this->dbCnx->prepare("INSERT INTO dvd (id, product_id, size) VALUES (?, ?, ?)");
+            $stm->execute([$this->id, $this->product_id, $this->size]);
+        }
+        catch(Expection $e){
+            return $e->getMessage();
+        }
+    }
+
+    public function fetchAll(){
+        try{
+            $stm = $this->dbCnx->prepare("SELECT * FROM dvd");
+            $stm->execute();
+            return $stm->fetchAll();
+        }
+        catch(Expection $e){
+            return $e->getMessage();
+        }
+    }
+
+    public function fetchOne(){
+        try{
+            $stm = $this->dbCnx->prepare("SELECT * FROM dvd WHERE id=?");
+            $stm->execute([$this->size]);
+            return $stm->fetchAll();
+        }
+        catch(Expection $e){
+            return $e->getMessage();
+        }
+    }
+
+    public function delete(){
+        try{
+            $stm = $this->dbCnx->prepare("DELETE FROM dvd WHERE id=?");
+            $stm->execute([$this->size]);
+            return $stm->fetchAll();
+        }
+        catch(Expection $e){
+            return $e->getMessage();
+        }
+    }
+
+    public function setSize($size){
+        $this->size = $size;
+    }
+    public function getSize(){
+       return $this->size;
+    }
+
+    public function setID($id){
+        $this->id = $id;
+    }
+    public function getID(){
+       return $this->id;
+    }
+}
+
+class Furniture extends addConfig{
+    private $height;
+    private $width;
+    private $length;
+
+    public function setHeight($height){
+        $this->height = $height;
+    }
+    public function getHeight(){
+       return $this->height;
+    }
+       public function setWidth($width){
+        $this->width = $width;
+    }
+    public function getWidth(){
+       return $this->width;
+    }
+       public function setLength($length){
+        $this->length = $length;
+    }
+    public function getLength(){
+       return $this->length;
+    }
+}
+
+class Book extends addConfig {
+    private $weight;
+
+    public function setWeight($weight){
+        $this->weight = $weight;
+    }
+    public function getWeight(){
+       return $this->weight;
     }
 }
 
--- php/addProcess.php
+++ php/addProcess.php
@@ -1,7 +1,9 @@
 <?php
+include 'ChromePhp.php';
 
     if(isset($_POST['send'])){
         require_once('addConfig.php');
+        
         $sc = new addConfig();
 
         $sc->setSKU($_POST['sku']);
@@ -9,9 +11,17 @@
         $sc->setPrice($_POST['price']);
         $sc->insertData();
 
+        $aaa = $sc->getID();
+        ChromePhp::log($aaa);
+
+        $scDvd = new Dvd();
+        $scDvd->setSize($_POST['size']);
+        $scDvd->insertData();
 
     }
 
-    header('Location: /php/index.php');
+    // header('Location: /php/index.php');
+
+
 
 ?>
